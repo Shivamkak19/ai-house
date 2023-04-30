4c6fc61d7384055cca189a13f791ff71c2d25e95
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2023-04-29 21:26:36 -0400

    adding reveal.js to dist

--- house/vite.config.cjs
+++ house/vite.config.cjs
@@ -1,13 +1,62 @@
-module.exports = {
-    build: {
-      rollupOptions: {
-        input: {
-          main: './index.html',
-          aitt: './Framework/aitt.html',
-          resources: './Framework/resources.html',
-          test: './test.html'
+// module.exports = {
+//     build: {
+//       rollupOptions: {
+//         input: {
+//           main: './index.html',
+//           aitt: './Framework/aitt.html',
+//           resources: './Framework/resources.html',
+//           home: './Framework/home.html'
 
-        }
-      }
-    }
-  }
+//         }
+//       }
+//     }
+//   }
+
+
+//   import { defineConfig } from 'vite';
+// import copy from 'rollup-plugin-copy';
+
+// module.exports = defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: './index.html',
+//         aitt: './Framework/aitt.html',
+//         resources: './Framework/resources.html',
+//         home: './Framework/home.html'
+//       },
+//       plugins: [
+//         copy({
+//           targets: [
+//             { src: './reveal.js-master/**/*', dest: 'dist' },
+//           ],
+//           // Other options...
+//         }),
+//       ],
+//     }
+//   }
+// });
+
+const copy = require('rollup-plugin-copy');
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        aitt: './Framework/aitt.html',
+        resources: './Framework/resources.html',
+        home: './Framework/home.html'
+      },
+      plugins: [
+        copy({
+          targets: [
+            { src: 'reveal.js-master/**/*', dest: 'dist' },
+          ],
+          // Other options...
+        }),
+      ],
+    },
+  },
+};
+
