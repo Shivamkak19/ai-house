9f443c7e16d7be167296a7fbe6e2268e7970f97e
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2023-04-29 21:07:22 -0400

    Created vite.config.cjs file to add subpages to build

---
+++ house/vite.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    build: {
+      rollupOptions: {
+        input: {
+          main: './index.html',
+          aitt: './Framework/aitt.html',
+          resources: './Framework/resources.html',
+          test: './test.html'
+
+        }
+      }
+    }
+  }
